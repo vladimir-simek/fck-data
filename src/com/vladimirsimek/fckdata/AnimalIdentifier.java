da6977f033e85c70f691580c0dd2a0682aaa7823
vladimir-simek <minervladik@gmail.com>
2021-03-02 12:24:42 +0100

    new type

---
+++ src/com/vladimirsimek/fckdata/AnimalIdentifier.java
@@ -0,0 +1,13 @@
+package com.vladimirsimek.fckdata;
+
+public class AnimalIdentifier {
+
+    public int id;
+    public int type;
+    public String name;
+    public int age;
+    public char gender;
+    public char isAggressive;
+    public char isFriendly;
+
+}
